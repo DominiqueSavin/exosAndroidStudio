9c5627af0d53c600aad0e002d08a934ad6fa4b20
DominiqueSavin <savin.dominique@gmail.com>
2023-08-24 11:48:07 +0200

    first commit

---
+++ app/src/main/java/com/example/app1_3/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.app1_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.app1_3.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    //Nécessite la modif du buil.gradle.kts(Module
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        this.InitHandlers();
+
+
+
+    }
+
+    private void InitHandlers(){
+        binding.btnLogin.setOnClickListener(this::clickLogin);
+        binding.btnReset.setOnClickListener(this::clickReset);
+    }
+
+    private void clickLogin(View view){
+        String username  = binding.usernameField.getText().toString();
+        String password = binding.passwordField.getText().toString();
+
+        Log.d(TAG,String.format("usename='%s', Password='%s'",username,password));
+    }
+
+    private void clickReset(View view){
+        binding.usernameField.setText("");
+        binding.passwordField.setText("");
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
